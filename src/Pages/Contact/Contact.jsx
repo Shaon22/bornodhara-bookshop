bcb7aa99356a15c7735f536aef18d47deb7a32a9
Shaon22 <shaonroy11@gmail.com>
2024-01-12 23:54:46 +0600

    added some content in contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,8 +1,30 @@
 
 const Contact = () => {
     return (
-        <div>
-            Contact
+        <div className="">
+            <div className="text-center">
+                <h1>THE ATHENIUM</h1>
+                <h1>Books All You Need</h1>
+            </div>
+            <div className="mt-10">
+            <h1 className='text-3xl font-bold text-center mt-10'>Our Team</h1>
+                <div className="mt-10">
+                    <div className="text-center">
+                        <img className="mx-auto rounded-xl" src="https://i.ibb.co/v3RrVLD/istockphoto-1096419446-612x612.jpg" alt="" />
+                       <h1>Shaon Polock Roy</h1>
+                       <h1>CEO</h1>
+                        <h1>THE ATHENIUM</h1>
+                    </div>
+                    <div>
+                        <img src="https://i.ibb.co/n3jhmCY/istockphoto-1139722168-1024x1024.jpg" alt="" />
+                        <h1>William Bistro</h1>
+                        <h1>Product Manager</h1>
+                        <h1>THE ATHENIUM</h1>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+
         </div>
     );
 };

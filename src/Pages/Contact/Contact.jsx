0571c41a9576fcd311d5740fe478705762eb5b02
Shaon22 <shaonroy11@gmail.com>
2024-01-12 23:59:25 +0600

    added some changes in contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -8,8 +8,8 @@ const Contact = () => {
             </div>
             <div className="mt-10">
             <h1 className='text-3xl font-bold text-center mt-10'>Our Team</h1>
-                <div className="mt-10">
-                    <div className="text-center">
+                <div className="mt-10 grid grid-cols-3">
+                    <div className="text-center cols-span-3">
                         <img className="mx-auto rounded-xl" src="https://i.ibb.co/v3RrVLD/istockphoto-1096419446-612x612.jpg" alt="" />
                        <h1>Shaon Polock Roy</h1>
                        <h1>CEO</h1>
@@ -21,7 +21,18 @@ const Contact = () => {
                         <h1>Product Manager</h1>
                         <h1>THE ATHENIUM</h1>
                     </div>
-                    <div></div>
+                    <div>
+                    <img src="https://i.ibb.co/BLSFjg5/rsz-looks-into-camera-young-man-casual-clothes-standing-indoors-studio-146671-59953.jpg" alt="" />
+                        <h1>Bill Johnson</h1>
+                        <h1>Assistant Product Manager</h1>
+                        <h1>THE ATHENIUM</h1>
+                    </div>
+                    <div>
+                    <img src="https://i.ibb.co/BLSFjg5/rsz-looks-into-camera-young-man-casual-clothes-standing-indoors-studio-146671-59953.jpg" alt="" />
+                        <h1>Bill Johnson</h1>
+                        <h1>Assistant Product Manager</h1>
+                        <h1>THE ATHENIUM</h1>
+                    </div>
                 </div>
             </div>
 

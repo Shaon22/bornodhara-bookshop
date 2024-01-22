92490c89ae7194e21939181003fdd8934c0073e6
Shaon22 <shaonroy11@gmail.com>
2024-01-22 23:39:47 +0600

    fixed button alignment issue

--- src/Pages/AllBooks/AllBooks.jsx
+++ src/Pages/AllBooks/AllBooks.jsx
@@ -6,19 +6,19 @@ const AllBooks = () => {
         <div className="p-5 sm:p-0 grid grid-cols-1 sm:grid-cols-4 gap-5">
             {
                 allBooks.map(allBook =>
-                    <div className="p-5 sm:p-3 shadow-2xl rounded text-center sm:text-left  " key={allBook._id}>
+                    <div className="p-5 relative sm:p-3 shadow-2xl rounded text-center sm:text-left  " key={allBook._id}>
                         <div>
                             <img className="h-[350px] mx-auto sm:mx-0" src={allBook.imageURL} alt="" />
                         </div>
-                        <div>
+                        <div className="mb-2">
                             <h1 className="text-2xl font-bold mt-5">{allBook.name}</h1>
                             <h1 className="font-medium mt-1">Author: {allBook.author}</h1>
                             <h1 className=" font-medium mt-1">Category: {allBook.category}</h1>
                             <h1 className=" font-medium mt-1">Ratings :{allBook.ratings}</h1>
 
                         </div>
-                        <Link to={`/details/${allBook._id}`}>
-                        <button className="flex items-center btn btn-sm bg-cyan-400 text-white">
+                        <Link  to={`/details/${allBook._id}`}>
+                        <button className="flex absolute bottom-2 items-center btn btn-sm bg-cyan-400 text-white">
                         <CgDetailsMore className="text-lg" />
                         Details
                         </button>
